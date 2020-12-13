0b1640185a500559a1fd8e9ea757d0f7b9632299
JulianCichorUP <73111644+JulianCichorUP@users.noreply.github.com>
2020-12-13 21:59:33 +0100

    Poprawa zadania

--- Word guessing.py
+++ Word guessing.py
@@ -1,9 +1,8 @@
 word = 'slowo'
-guess = ''
 for i in range(5):
     guess = input("Zgadnij slowo: ")
-    if guess != word:
-        print("Odpowiedz nieprawidlowa.")
-    else:
+    if guess == word:
         print("Odpowiedz prawidlowa.")
         break
+else:
+    print("Odpowiedz nieprawidlowa.")
